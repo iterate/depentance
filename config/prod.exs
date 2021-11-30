917979cb1c91d186f3a294ba0b427593a6efecb5
jraregris <jraregris@gmail.com>
2021-11-30 21:20:23 +0100

    Set iterapp url as prod endpoint

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :depentance, DepentanceWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "depentance.app.iterate.no", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
